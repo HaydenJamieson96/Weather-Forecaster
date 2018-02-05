ac722b636ab873a8dbdfe79537a78300f22caf46
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-02-05 11:10:03 +0000

    Quick TblView delegate/data source config

--- Weather-Forecaster/Controller/WeatherVC.swift
+++ Weather-Forecaster/Controller/WeatherVC.swift
@@ -19,11 +19,27 @@ class WeatherVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     
+}
 
-
+extension WeatherVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    
 }
 
---
+++ Weather-Forecaster/View/WeatherCell.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherCell.swift
+//  Weather-Forecaster
+//
+//  Created by Hayden Jamieson on 05/02/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    @IBOutlet weak var weatherImg: UIImageView!
+    @IBOutlet weak var dayLbl: UILabel!
+    @IBOutlet weak var weatherTypelbl: UILabel!
+    @IBOutlet weak var highTempLbl: UILabel!
+    @IBOutlet weak var lowTempLbl: UILabel!
+    
+
+}
